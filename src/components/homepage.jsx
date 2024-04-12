b18a2a39696d51ff075c508c76b8ee44819a721a
deapthspin <thedeapthspin@gmail.com>
2024-04-12 13:18:24 +0800

    sfd

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -6,6 +6,7 @@ function Homepage() {
     const [alpha, setAlpha] = useState(0)
     const [beta, setBeta] = useState(0)
     const [gamma, setGamma] = useState(0)
+    const [perms, setPerms] = useState(false)
 
     
 
@@ -16,7 +17,7 @@ function Homepage() {
             .then((state) => {
               if (state === 'granted') {
                 window.addEventListener('deviceorientation', handleOrientation);
-                // setStarted(true)
+                setPerms(true)
               } else {
                 console.error('Request to access the orientation was rejected');
               }
@@ -24,13 +25,11 @@ function Homepage() {
             .catch(console.error);
         } else {
           // Handle regular non iOS 13+ devices.
+          setPerms(true)
           window.addEventListener('deviceorientation', handleOrientation);
         }
       }
     
-      useEffect(() => {
-        handleClick()
-      }, [])
       
       function handleOrientation(event) {
         const a = event.alpha;
@@ -42,11 +41,19 @@ function Homepage() {
       }
     return(
         <div className='App'>
-            {alpha > 55 && beta < 15 && gamma < -55 ? <div className='center'>
-                <button onClick={(e) => navigate('/rooms')} >multiplayer</button>
-                <button onClick={(e) => navigate('/main')} >learn</button>
-                <button onClick={(e) => navigate('/singlechallenge')} >solo challenge</button>
-            </div> : <h1>tilt your phone to landscape</h1>}
+          <div>
+            {!perms && <button onClick={(e) => handleClick()}>enable gyro</button>}
+            <br/>
+            <br/>
+            
+            {!perms && <h3>the app requires the permission to use gyro to function</h3>}            
+          </div>
+
+            {perms && <div className='center'>
+              <button onClick={(e) => navigate('/main')} >learn</button>
+              <button onClick={(e) => navigate('/singlechallenge')} >solo</button>
+              <button onClick={(e) => navigate('/rooms')} >group</button>
+            </div>}
             
 
         </div>
